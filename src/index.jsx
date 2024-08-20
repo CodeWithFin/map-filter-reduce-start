e2800ba3bb8e3e28ed60aab6af81e75adab598d3
Finley <finley.mwachia12@gmail.com>
2024-08-20 09:11:33 +0300

    add files to project

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
